238572083e7a6237d7d7aa9daf769cb2ad87b4e5
Matias <matias.pz@proton.me>
2022-08-31 02:30:54 -0300

    linux: give ENOTTY the UnsupportedOperation error kind

--- src/platforms/linux/error.rs
+++ src/platforms/linux/error.rs
@@ -14,7 +14,7 @@ impl From<nix::errno::Errno> for ErrorKind {
     fn from(e: nix::errno::Errno) -> Self {
         use nix::errno::Errno;
         match e {
-            Errno::ENOSYS | Errno::EOPNOTSUPP => Self::UnsupportedOperation,
+            Errno::ENOSYS | Errno::EOPNOTSUPP | Errno::ENOTTY => Self::UnsupportedOperation,
             Errno::EPERM | Errno::EACCES => Self::AccessDenied,
             Errno::ENOENT => Self::NotFound,
             Errno::ENAMETOOLONG => Self::InvalidPath,

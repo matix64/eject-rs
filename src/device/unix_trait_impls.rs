ea28943636e82b9b596890787f9a4f799b2f40c7
Matias <matias.pz@proton.me>
2022-09-04 05:57:55 -0300

    fixed Device <--> unix file descriptor conversions

--- src/device/unix_trait_impls.rs
+++ src/device/unix_trait_impls.rs
@@ -1,28 +1,23 @@
 use super::Device;
 use crate::platform::device::DeviceHandle;
-use std::os::unix::prelude::*;
+use std::{mem::forget, os::unix::prelude::*};
 
 impl AsRawFd for Device {
+    #[inline]
     fn as_raw_fd(&self) -> RawFd {
         self.handle.0
     }
 }
 
 impl AsFd for Device {
+    #[inline]
     fn as_fd(&self) -> BorrowedFd<'_> {
-        unsafe { BorrowedFd::borrow_raw(self.handle.0) }
-    }
-}
-
-impl From<OwnedFd> for Device {
-    fn from(fd: OwnedFd) -> Self {
-        Self {
-            handle: DeviceHandle(fd.as_raw_fd()),
-        }
+        unsafe { BorrowedFd::borrow_raw(self.as_raw_fd()) }
     }
 }
 
 impl FromRawFd for Device {
+    #[inline]
     unsafe fn from_raw_fd(fd: RawFd) -> Self {
         Self {
             handle: DeviceHandle(fd),
@@ -30,8 +25,18 @@ impl FromRawFd for Device {
     }
 }
 
+impl From<OwnedFd> for Device {
+    #[inline]
+    fn from(fd: OwnedFd) -> Self {
+        unsafe { Self::from_raw_fd(fd.into_raw_fd()) }
+    }
+}
+
 impl IntoRawFd for Device {
+    #[inline]
     fn into_raw_fd(self) -> RawFd {
-        self.handle.0
+        let fd = self.as_raw_fd();
+        forget(self);
+        fd
     }
 }
